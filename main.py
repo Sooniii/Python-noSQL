abe9f73d234bb04b3d0d3fee0a6777af83376f79
Alban <alban@MacBook-Pro-de-Admin.local>
2021-10-11 15:47:12 +0200

    v.0.0.1 print la list des users sur le site

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
-
+import os
 
 from flask import Flask
+from flask import request
+from flask import make_response
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/user", methods=["GET"])
+def show_users():
+    reponse = ""
+    folder = os.listdir("./user")
+    #print(folder)
 
-app.run(
-    host = "0.0.0.0",
-    port= "8001",
-    debug= True
+    for item in folder:
+        print(item)
+        texte = open("./user/"+item, 'r')
+        user = texte.readline()
+        reponse = reponse + user
+        texte.close()
+
+    return make_response(reponse,200)
 
 
 
+@app.route("/", methods=["GET","POST"])
+def acceuil():
 
-)
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        pass
 
+    print(f"arguments : {request.args}")
+    print(f"body: {request.get_json()}")
+
+    body = " Je fonctionne "
+    return make_response(body,200)
+
+if(__name__) == '__main__':
+    app.run(
+        host = "0.0.0.0",
+        port=8081,
+        debug=True,
+    )
