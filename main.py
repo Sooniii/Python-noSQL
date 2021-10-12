0bd56a3f06ca73ac7531d52ef3ffa322d58f9155
Alban <alban@MacBook-Pro-de-Admin.local>
2021-10-12 09:37:56 +0200

    v.0.0.2 reglage de beug

--- main.py
+++ main.py
@@ -6,40 +6,61 @@ from flask import make_response
 
 app = Flask(__name__)
 
-@app.route("/user", methods=["GET"])
-def show_users():
-    reponse = ""
-    folder = os.listdir("./user")
-    #print(folder)
 
-    for item in folder:
-        print(item)
-        texte = open("./user/"+item, 'r')
-        user = texte.readline()
-        reponse = reponse + user
-        texte.close()
-
-    return make_response(reponse,200)
-
-
-
-@app.route("/", methods=["GET","POST"])
+# @app.route("/", methods=["GET","POST"])
 def acceuil():
-
     if request.method == "GET":
         pass
     elif request.method == "POST":
         pass
 
-    print(f"arguments : {request.args}")
+    print(f"arguments : {request.args['id']}")
     print(f"body: {request.get_json()}")
 
     body = " Je fonctionne "
-    return make_response(body,200)
+    return make_response(body, 200)
 
-if(__name__) == '__main__':
+
+"""
+affiche les utilisateur
+params:none
+return: une reponse avec le nom utilisateur
+
+
+"""
+
+
+@app.route("/user", methods=["GET"])
+def show_users():
+    reponse = ""
+    folder = os.listdir("./user")
+    # print(folder)
+
+    for item in folder:
+        texte = open("./user/" + item, 'r')
+        user = texte.readline()
+        reponse = reponse + user
+        texte.close()
+
+    return make_response(reponse, 200)
+
+
+"""
+ajoute un utilisateur
+params: newUser(un json avec le nom et le prenom d'un nouvel utilisateur)
+"""
+
+
+@app.route("/adduser", methods=["POST"])
+def add_user():
+    name = request.get_json()
+    print(name)
+    return make_response(name, 200)
+
+
+if (__name__) == '__main__':
     app.run(
-        host = "0.0.0.0",
+        host="0.0.0.0",
         port=8081,
         debug=True,
     )
